484b39f51dc73d429e79b1ca8f8413418ba020aa
Vladosbandosss <dasistperfektos@gmail.com>
2021-12-09 09:35:13 +0300

    new task change color

---
+++ hometaskbygit/Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    private MeshRenderer _mesh;
+    
+    private  void Start()
+    {
+        _mesh = GetComponent<MeshRenderer>();
+    }
+
+   
+   private void Update()
+    {
+        _mesh.material.color=Color.Lerp (Color.white, Color.black, Mathf.Abs(Mathf.Sin(Time.time)));
+    }
+}
